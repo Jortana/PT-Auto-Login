965530ba4b6c9e41e327f18d6f8e3dbdfc51f374
Rory <jyljensen@outlook.com>
2024-10-20 21:54:04 +0800

    feat: 支持 cookie 登录的站点

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+type SiteConfig = {
+  name: string
+  url: string
+  cookies: string
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request, env, ctx): Promise<Response> {
+    try {
+      const siteConfigs = await env.PAL.get<SiteConfig[]>('config', 'json')
+      if (!siteConfigs) {
+        return new Response('No config found', { status: 404 })
+      }
+
+      const taskConfigs = siteConfigs.map((siteConfig) => {
+        const delayMillis = Math.floor(Math.random() * 300000)
+        return { ...siteConfig, delay: delayMillis }
+      })
+
+      console.log('[START] Start login tasks.')
+
+      const tasks = taskConfigs.map((taskConfig) => {
+        return async () => {
+          const { name, url, cookies, delay } = taskConfig
+          console.log(
+            `[LOGIN] Start login task for ${name} in ${delay / 1000}s.`,
+          )
+          await sleep(delay)
+          const response = await fetch(url, {
+            headers: {
+              Cookie: cookies,
+            },
+          })
+          if (response.status === 200) {
+            console.log(`[LOGIN SUCCESS] ${name} login success.`)
+          } else {
+            console.error(`[LOGIN FAILED] ${name} login failed.`)
+          }
+        }
+      })
+
+      await Promise.allSettled(tasks.map((task) => task()))
+
+      console.log('[COMPLETE] Tasks completed.')
+
+      return new Response('Login tasks completed.', { status: 200 })
+    } catch (error) {
+      return new Response(
+        error instanceof Error ? error.message : JSON.stringify(error),
+        { status: 500 },
+      )
+    }
+  },
+} satisfies ExportedHandler<Env>
+
+async function sleep(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
