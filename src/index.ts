fc412f0d795c3376d035df33dd0c13af0567ac87
Rory <jyljensen@outlook.com>
2024-10-25 11:23:46 +0800

    feat: export scheduled function

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,7 @@ export default {
         return new Response('No config found', { status: 404 })
       }
 
-      const taskConfigs = siteConfigs.map((siteConfig) => {
-        // const delayMillis = Math.floor(Math.random() * 300000)
-        const delayMillis = Math.floor(Math.random() * 60000)
-        return { ...siteConfig, delay: delayMillis }
-      })
-
-      console.log('[START] Start login tasks.')
-
-      await runCookieTasks(taskConfigs)
-
-      console.log('[COMPLETE] Tasks completed.')
+      await autoLogin(siteConfigs)
 
       return new Response('Login tasks completed.', { status: 200 })
     } catch (error) {
@@ -34,4 +24,29 @@ export default {
       )
     }
   },
+  async scheduled(event, env, ctx) {
+    try {
+      const siteConfigs = await env.PAL.get<SiteConfig[]>('config', 'json')
+      if (!siteConfigs) {
+        throw new Error('No config found')
+      }
+      await autoLogin(siteConfigs)
+    } catch (error) {
+      console.error(error)
+    }
+  },
 } satisfies ExportedHandler<Env>
+
+async function autoLogin(siteConfigs: SiteConfig[]) {
+  const taskConfigs = siteConfigs.map((siteConfig) => {
+    // const delayMillis = Math.floor(Math.random() * 300000)
+    const delayMillis = Math.floor(Math.random() * 60000)
+    return { ...siteConfig, delay: delayMillis }
+  })
+
+  console.log('[START] Start login tasks.')
+
+  await runCookieTasks(taskConfigs)
+
+  console.log('[COMPLETE] Tasks completed.')
+}
